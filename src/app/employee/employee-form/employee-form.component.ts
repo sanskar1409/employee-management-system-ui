ff92fba780f4cbddf08c6cfe81fc20aa7ba4d4a4
sanskar1409 <jonnalagaddasanskar@gmail.com>
2025-08-07 11:36:20 +0530

    Changes

--- src/app/employee/employee-form/employee-form.component.ts
+++ src/app/employee/employee-form/employee-form.component.ts
@@ -1,11 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { EmployeeService } from '../employee.service';
+import { Employee } from '../employee.model';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-employee-form',
-  imports: [],
-  templateUrl: './employee-form.component.html',
-  styleUrl: './employee-form.component.css'
+  templateUrl: './employee-form.component.html'
 })
-export class EmployeeFormComponent {
+: Employee = { name: '', email: '', department: '', salary: 0 };
+  isEdit = false;
 
+  constructor(private service: EmployeeService, private route: ActivatedRoute, private router: Router) {}
+
+  ngOnInit(): void {
+    const.navigate(['/']));
+    } else {
+      this.service.create(this.employee).subscribe(() => this.router.navigate(['/']));
+    }
+  }
 }
--- src/app/employee/employee-list/employee-list.component.ts
+++ src/app/employee/employee-list/employee-list.component.ts
@@ -1,11 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { EmployeeService } from '../employee.service';
+import { Employee } from '../employee.model';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-employee-list',
-  imports: [],
-  templateUrl: './employee-list.component.html',
-  styleUrl: './employee-list.component.css'
+  templateUrl: './employee-list.component.html'
 })
-export class EmployeeListComponent {
+export class EmployeeListComponent implements OnInit {
+  employees: Employee[] = [];
 
+  constructor(private service: EmployeeService, private router: Router) {}
+
+  ngOnInit(): void {
+    this.service.getAll().subscribe(data => this.employees = data);
+  }
+
+  delete(id: number): void {
+    this.service.delete(id).subscribe(() => {
+      this.employees = this.employees.filter(e => e.id !== id);
+    });
+  }
+
+  edit(id: number): void {
+    this.router.navigate(['/edit', id]);
+  }
+
+  add(): void {
+   add']);
+  }
 }
